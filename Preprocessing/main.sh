0d4d70c6982112911c2fa22e0871dc08a8fc1154
Emile Aydar <114087019+xXMagIkZzR4mBOXx@users.noreply.github.com>
2023-08-19 22:23:36 +0200

    Update main.sh

--- Preprocessing/main.sh
+++ Preprocessing/main.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+#SBATCH --partition=hard
+#SBATCH --job-name=main
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=11000
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+
+python main.py\
+    tiff_path input\
+    window_shape 16 16 16\
+    --step 8\
+    --clip_limit 0.01\
+    --save_path 'output.tif'\
+    --debug\
+    
